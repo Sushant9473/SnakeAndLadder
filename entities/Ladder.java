5f2f671b1036327abb1dfa32c92171e88f9b2e5d
Sushant Singh Chauhan <sushant@Sushants-MacBook-Air.local>
2025-09-22 11:21:04 +0530

    Completed Assignment

---
+++ entities/Ladder.java
@@ -0,0 +1,17 @@
+public class Ladder extends BoardEntity {
+
+	public Ladder(int start, int end) {
+		super(start, end);
+	}
+
+	@Override
+	public String getEncounterMessage() {
+		return "Yeyyy!! You have a stair to climb :) ";
+	}
+
+	@Override
+	public String getString() {
+		return "L(" + this.getEnd() + ")  ";
+	}
+
+}
